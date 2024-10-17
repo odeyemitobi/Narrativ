595c361c18fc43020c6122fad46bc1f331dc54ae
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2024-10-17 07:58:27 +0100

    debug off

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ with app.app_context():
     print("Database tables created!")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
     # app.run(debug=True)
